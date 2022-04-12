ce600912db7d8582a794f4aebc375b3cf645be07
Anantha S N <ananthasn@Ananthas-MacBook-Pro.local>
2022-04-12 16:15:34 -0700

    Grapgql update

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-import git_response_repo as repo
-import sample_git_api as api
+from distutils.command.config import config
+import mongo_data_handler
+import fetch_data as data
+import sys
+import json
+
 def main():
-    response = api.SampleGitApi().getUser()
-    res = repo.GitResponseRepo().save_response(response)
-    if res:
-        print("saved")
-    else:
-        print("not saved")
+    f = open('./config.json')
+    config = json.load(f)
+    f.close()
+    organization = config["COMPANY"]
+    response_org_id  = data.fetch_git_data().get_organization(organization)
+
+    # # reading from json payload
+    # payload_repos = []
+    # if response_org_id:
+    #     for repo in payload_repos:
+    #         ## 1
+    #         issues_reposne = data.fetch_git_data().get_perceval_data("Issue", organization, repo, from_date, to_date)
+    #         ## 2
+    #         commit_reposne = data.fetch_git_data().get_commit_data(repo_url)
+
+    # ## extract data from issues
+
+    # count = mongo_data_handler.MongoDataHandler().extract_user_data_from_issues()
+    # count1 = mongo_data_handler.MongoDataHandler().extract_user_data_from_commits()
+    # print(count1)
+    # print(count)
+
+
+
 
 if __name__ == "__main__":
     main()
